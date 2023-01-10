575177a04b7adb874710cbd74b9261fc433dabcf
vientran199 <68174942+vientran199@users.noreply.github.com>
2023-01-10 11:03:46 +0700

    Install @fluentui/react @pnp/spfx-controls-react @pnp/sp

--- src/webparts/webPartTabs/components/WebPartTabs.tsx
+++ src/webparts/webPartTabs/components/WebPartTabs.tsx
@@ -1,43 +1,28 @@
 import * as React from 'react';
 import styles from './WebPartTabs.module.scss';
 import { IWebPartTabsProps } from './IWebPartTabsProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { Pivot, PivotItem, Label } from '@fluentui/react'
 
-export default class WebPartTabs extends React.Component<IWebPartTabsProps, {}> {
-  public render(): React.ReactElement<IWebPartTabsProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
-
-    return (
-      <section className={`${styles.webPartTabs} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
+export default function WebPartTabs({ }: IWebPartTabsProps) {
+  return (
+    <>
+      <Pivot aria-label="Basic Pivot Example">
+        <PivotItem
+          headerText="My Files"
+          headerButtonProps={{
+            'data-order': 1,
+            'data-title': 'My Files Title',
+          }}
+        >
+          <Label >Pivot #1</Label>
+        </PivotItem>
+        <PivotItem headerText="Recent">
+          <Label>Pivot #2</Label>
+        </PivotItem>
+        <PivotItem headerText="Shared with me">
+          <Label>Pivot #3</Label>
+        </PivotItem>
+      </Pivot>
+    </>
+  )
 }
